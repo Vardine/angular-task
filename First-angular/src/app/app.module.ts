54374de61fd055885e53ed67de2f89690ee885f5
Vardi <vardine.hayrapetyan@gmail.com>
2018-07-20 18:48:17 +0400

    Added Login form

--- First-angular/src/app/app.module.ts
+++ First-angular/src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path:'login',
+        component:LoginComponent
+      },
+      {
+        path:'',
+        component:HomeComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
